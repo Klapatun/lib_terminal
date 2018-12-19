2043a0b7ec8742aa7e8aa2296aca15acfb4dca2d
Anton Klapatun <klapatun@zao-rim.ru>
2018-12-19 18:09:57 +0600

    Added terminal structure description and its initialization

--- inc/terminal.h
+++ inc/terminal.h
@@ -1,4 +1,39 @@
 #ifndef TERMINAL_H_
 #define TERMINAL_H_
 
+/*Include*/
+/*,,,,,,,.....................................................................*/
+#include "terminal.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+/*____________________________________________________________________________*/
+
+
+/*Define*/
+/*............................................................................*/
+#define TERMINAL_SIZE_MESSAGE        64
+#define TERMINAL_SIZE_COMMAND        16
+#define TERMINAL_SIZE_DATA           ((TERMINAL_SIZE_MESSAGE)-(TERMINAL_SIZE_COMMAND))
+
+#define TERMINAL_STATE_BUSY          0
+#define TERMINAL_STATE_FREE          1
+#define TERMINAL_STATE_OVERFLOW      2
+/*____________________________________________________________________________*/
+
+
+/*Struct*/
+/*............................................................................*/
+
+typedef struct terminal_struct{
+  uint8_t state;
+  char* msg;
+  uint8_t len_command;
+  uint8_t len_data;
+  char* command;
+  char* data;
+}terminal_t ;
+
+/*____________________________________________________________________________*/
+
 #endif /*TERMINAL_H_*/
--- src/terminal.c
+++ src/terminal.c
@@ -1,2 +1,21 @@
+/**
+ * @file
+ * Terminal implementation for any microcontrollers.
+ *
+ */
+
+
 #include "terminal.h"
 
+
+void terminal_init(terminal_t *term) {
+
+  term->len_command=0;
+  term->len_data=0;
+  term->msg = (char*)malloc(TERMINAL_SIZE_MESSAGE);
+  term->command = (char*)malloc(TERMINAL_SIZE_COMMAND);
+  term->data = (char*)malloc(TERMINAL_SIZE_DATA);
+  term->state = TERMINAL_STATE_FREE;
+}
+
+/*****************************END OF FILE**************************************/
