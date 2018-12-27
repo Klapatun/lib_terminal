24464d3cfa5cd59bc88e29b05f50dc9a12dcdaad
Anton Klapatun <klapatun@zao-rim.ru>
2018-12-27 18:06:46 +0600

    Added function t_tcp_command, which will be used to execute tcp commands

--- modules/lwip/terminal_lwip.c
+++ modules/lwip/terminal_lwip.c
@@ -12,6 +12,29 @@
 
 /*____________________________________________________________________________*/
 
+void t_tcp_command(char* string, size_t flagCommand) {
+  
+  size_t idx=0;
+  
+  while (string[idx] == ' ') {
+    idx++;
+  }
+  
+  if (flagCommand == TERMINAL_TCP_CONNECT) {
+    t_transmit("Connect \n", 9);
+  }
+  else if (flagCommand == TERMINAL_TCP_DISCONNECT) {
+    t_transmit("Disconnect \n", 12);
+  }
+  else if (flagCommand == TERMINAL_TCP_LISTEN) {
+    t_transmit("Listen \n", 8);
+  }
+  else {
+    
+  }
+    
+}
+
 size_t t_parser_data(char* string, size_t numStr) {
  
   size_t n = strcspn(string, " ");
@@ -26,6 +49,26 @@ size_t t_parser_data(char* string, size_t numStr) {
     }
     else if (!(strcmp("tcp", tmp_str))) {
        t_transmit("Tcp \n", 5);
+       
+       n = strcspn(string, "-");
+       switch(string[n+1]) {
+         case 'c': {
+           t_tcp_command(&string[n+2], TERMINAL_TCP_CONNECT);
+           break;
+         }
+         case 'd': {
+           t_tcp_command(&string[n+2], TERMINAL_TCP_DISCONNECT);
+           break;
+         }
+         case 'l': {
+           t_tcp_command(&string[n+2], TERMINAL_TCP_LISTEN);
+           break;
+         }
+        default: {
+           t_tcp_command(&string[n+2], 4);
+        }
+       }
+//       t_tcp_command();
     }
     else {
       t_transmit("Unknown command \n", 17);
--- modules/lwip/terminal_lwip.h
+++ modules/lwip/terminal_lwip.h
@@ -1,6 +1,15 @@
 #ifndef TERMINAL_LWIP_H_
 #define TERMINAL_LWIP_H_
 
+/*Define*/
+/*............................................................................*/
+
+#define TERMINAL_TCP_CONNECT            0
+#define TERMINAL_TCP_DISCONNECT         1
+#define TERMINAL_TCP_LISTEN             2
+
+/*____________________________________________________________________________*/
+
 
 /*Function declaration*/
 /*............................................................................*/
